90fd42b7f9e3b0fda1ee344312d74094450f90f7
mohitsankholia <sankholia.mohit@gmail.com>
2022-02-03 11:56:51 +0530

    RouterDom

--- dlithe-sport/src/App.js
+++ dlithe-sport/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { NewTournament } from './NewTournament'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+
 
 const App=()=>{
   return(
-    <>
-        <NewTournament/>
-    </>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<h1>HOME Page</h1>}/>
+          <Route path="/new" exact element={<NewTournament/>}/>
+          <Route path="/old" exact element={<h1>OLD Page</h1>}/>
+        </Routes>
+      </BrowserRouter>
   )
 }
 
